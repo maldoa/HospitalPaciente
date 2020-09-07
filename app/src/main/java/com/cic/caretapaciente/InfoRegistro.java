b1f5ddaa641913d0b4354a519af55e32914188fc
Alberto Maldonado <amaldonador1300@alumno.ipn.mx>
2020-09-07 17:33:49 -0500

    registro info

---
+++ app/src/main/java/com/cic/caretapaciente/InfoRegistro.java
@@ -0,0 +1,14 @@
+package com.cic.caretapaciente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoRegistro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_registro);
+    }
+}
--- app/src/main/java/com/cic/caretapaciente/MainActivity.java
+++ app/src/main/java/com/cic/caretapaciente/MainActivity.java
@@ -1,14 +1,38 @@
 package com.cic.caretapaciente;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+        setContentView(R.layout.caratula);
+
+        new CountDownTimer(1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                setContentView(R.layout.activity_info_app);
+
+                }
+
+        }.start();
+
+    }
+    @Override
+    public void onClick(View view){
+        Intent intent = new Intent(MainActivity.this, InfoRegistro.class);
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, view, "simple_activity_transition");
+        startActivity(intent, options.toBundle());
     }
 }
