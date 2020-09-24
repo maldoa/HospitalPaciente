2fe1e2026f44a1719719b0d4ce02ee6e84560fad
Alberto Maldonado <amaldonador1300@alumno.ipn.mx>
2020-09-23 21:08:32 -0500

    formulario design la1

--- app/src/main/java/com/cic/caretapaciente/Registro.java
+++ app/src/main/java/com/cic/caretapaciente/Registro.java
@@ -5,18 +5,76 @@ import androidx.core.app.ActivityOptionsCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Registro extends AppCompatActivity implements View.OnClickListener {
+
+    // Datos peronales
+    private TextInputEditText nombreCompleto;
+    private TextInputEditText edad;
+    private TextInputEditText telefono;
+    private TextInputEditText peso;
+    private TextInputEditText altura;
+    private TextInputEditText fechaInicio;
+    private TextInputEditText calleDomicilio;
+    private TextInputEditText colonia;
+    private TextInputEditText cp;
+
+    //boton parte 1
+    private Button siguiente1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        // inicializar layout 1
+        nombreCompleto = (TextInputEditText) findViewById(R.id.campoNombreCompleto);
+        edad = (TextInputEditText) findViewById(R.id.campoEdad);
+        telefono  = (TextInputEditText) findViewById(R.id.campoTelefono);
+        peso = (TextInputEditText) findViewById(R.id.campoPeso);
+        altura = (TextInputEditText) findViewById(R.id.campoAltura);
+        fechaInicio = (TextInputEditText) findViewById(R.id.campoFechainicio);
+        calleDomicilio = (TextInputEditText) findViewById(R.id.campoCalleNumero);
+        colonia = (TextInputEditText) findViewById(R.id.campoColonia);
+        cp = (TextInputEditText) findViewById(R.id.campoCP);
+
+        siguiente1 = (Button) findViewById(R.id.botonSiguiente1);
+
+
+
+
     }
+
+
+
+
+
+
+    // llamar layout 2
     @Override
     public void onClick(View view){
-        setContentView(R.layout.activity_registro_2);
+        Pattern ps = Pattern.compile("^[a-zA-Z ]+$");
+        Matcher ms = ps.matcher(nombreCompleto.getText().toString());
+        boolean bs = ms.matches();
+        if (bs == false) {
+            nombreCompleto.setError("error type");
+            Toast.makeText(getApplicationContext(), "Please enter something !", Toast.LENGTH_LONG).show();
+        }
+        else {
+            setContentView(R.layout.activity_registro_2);
+        }
     }
 
     public void onClick2(View view){
---
+++ app/src/main/java/com/cic/caretapaciente/Sintomas.java
@@ -0,0 +1,13 @@
+package com.cic.caretapaciente;
+
+public class Sintomas {
+    private boolean cefalea;
+    private boolean tos;
+    private boolean dolorAbdominal;
+    private boolean dolorToracito;
+    private boolean vomito;
+    private boolean irritabilidad;
+    private boolean perdidaOlfatoGusto;
+
+
+}
